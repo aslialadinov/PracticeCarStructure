e5f1ca8268f6ef0e6e39579293438167ca977663
aslialadinov <asaladinov@gmail.com>
2023-03-31 19:05:53 -0400

    the engine specs has been added second commit

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,10 @@ public class Main {
         audi.color="balck";
         audi.model="A3";
         audi.power="5000cc";
+
+        Engine x=new Engine();
+        x.cylinder="8";
+        x.size="1212";
+        x.weight="14555";
     }
 }
